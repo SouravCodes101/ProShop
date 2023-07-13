4ac7a7729203e1abe09dc85e2245c775c779d232
Sourav Choudhury <saurabc987@gmail.com>
2023-07-13 21:22:39 +0530

    Setup MongoDB and Mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDb from "./config/db.js";
-import products from "./data/products.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const port = process.env.PORT || 5000;
 
-connectDb();//Connect to MongoDB
-
+connectDb(); //Connect to MongoDB
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
