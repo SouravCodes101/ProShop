4a1d74cad97ea950b85c7f8a6a8efbe9f35eefe3
Sourav Choudhury <saurabc987@gmail.com>
2023-07-22 10:54:57 +0530

    Implement Redux , Loader and Message Components

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -80,4 +80,4 @@ const orderSchema = new mongoose.Schema(
 
 const Order = mongoose.model("Order", orderSchema);
 
-export default Order;
+export default Order; 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,31 @@
-import { useEffect, useState } from "react";
 import React from "react";
 import { Row, Col } from "react-bootstrap";
-import axios from "axios";
 import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((p) => (
-          <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={p} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error?.data?.message || error.error}</Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((p) => (
+              <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={p} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
