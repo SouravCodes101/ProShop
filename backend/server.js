5f819b313b705164d2e6e7c08cc1597fd7d9d387
Sourav Choudhury <saurabc987@gmail.com>
2023-07-24 10:32:23 +0530

    Authenticate User

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const port = process.env.PORT || 5000;
 connectDb(); //Connect to MongoDB
 const app = express();
 
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
