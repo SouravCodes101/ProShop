67b6358cf862fc00a0a3f3dabd97740c9ce0f133
Sourav Choudhury <saurabc987@gmail.com>
2023-07-27 06:39:38 +0530

    Auth Middleware and Endpoint

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,20 +1,23 @@
-import bcrypt from "bcryptjs";
+import bcrypt from 'bcryptjs';
 
 const users = [
   {
-    name: "Admin User",
-    email: "admin@email.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'Admin User',
+    email: 'admin@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
   },
   {
-    name: "John Doe",
-    email: "john@email.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
   },
   {
-    name: "Jane Doe",
-    email: "jane@email.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'Jane Doe',
+    email: 'jane@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
   },
 ];
 
